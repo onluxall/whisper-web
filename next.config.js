81c22c879ad02dad5cd654acd1c634ea6ca877d9
Luxall <onluxall@gmail.com>
2025-05-19 17:34:06 +0200

    Update Vercel configuration and form logic

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/whisper_web' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/whisper_web/' : '',
   /* config options here */
 };
 
