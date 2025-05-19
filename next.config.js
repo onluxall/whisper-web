bf1a36ca61e316359ebddbaca138661c2dd26ac1
Luxall <onluxall@gmail.com>
2025-05-19 14:44:12 +0200

    Configure for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/whisper-web',
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
 };
 
